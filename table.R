959f34966ad4e64a6db793a6d8cad87b890ead14
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2022-08-09 17:28:34 +0300

    Update table.R

--- table.R
+++ table.R
@@ -27,7 +27,6 @@ data <- data %>%
 
 
 
-
 # Ищем пациентов, которые есть в базе ---------------------------------------------------------
 
 data_one_new <- data_redcap %>%
@@ -70,12 +69,19 @@ data_one <- data_redcap %>%
          outcome,
          outcome_date) %>%
   mutate(pat_fio = paste(last_name,str_sub(`first_name`,1,1),gsub("-", "",birthdate), sep = "_"))
-  data_one[data_one$record_id %in% data_one_new$record_id, 'relapse'] <-  '1' # исправляем ошибки рецидивов
+  data_one[data_one$record_id %in% data_one_new$record_id, 'relapse'] <-  '1'
   data_one[data_one$record_id %in% data_one_new$record_id, 'relapse_dt'] <- data_one_new$event_date
 data_double <- data_one %>% 
   full_join(data_double, by = 'pat_fio') %>% 
-  filter(!is.na(table_id) | (!is.na(record_id) & relapse == 1))
+  filter(!is.na(table_id) | (!is.na(record_id) & relapse == 1)) 
 
+data_time <- data_double %>% 
+  filter(!is.na(relapse_dt.x) & !is.na(ds_dt...12) & !is.na(date...13)) %>% 
+  add_column(diff_date_1 = difftime(.$relapse_dt.x, .$ds_dt...12, units = "day")) %>% 
+  add_column(diff_date_2 = difftime(.$relapse_dt.x, .$date...13, units = "day")) %>% 
+  select(pat_fio,diff_date_1,diff_date_2)
+
+data_double <- data_double %>% left_join(data_time) 
 
 # Переименовываем столбцы ------------
 labels <- c(record_id = 'record_id',
@@ -104,12 +110,14 @@ labels <- c(record_id = 'record_id',
             date...13 = 'Дата операции (таблица)',
             outcome.y = 'Исход (таблица)',
             outcome_date.y = 'Дата исхода (таблица)',
-            table_id = 'table_id')
+            table_id = 'table_id',
+            diff_date_1 = 'разница между датой диагноза и рецидива',
+            diff_date_2 = 'разница между датой операции и рецидива')
 
 data_double <- data_double %>% 
   select(record_id, table_id, pat_fio,	first_name,	last_name,	birthdate,	ds_dt,	date,	relapse,	relapse_dt.x,	mgroup.x,	
              outcome.x,	outcome_date.x,	first_name...1,	last_name...2,	birthdate...3,	ds_dt...4,	date...5,	relapse_dt.y,	mgroup.y,	
-             first_name...9,	last_name...10,	birthdate...11,	ds_dt...12,	date...13,	outcome.y,	outcome_date.y) # задаём порядок столбцов
+             first_name...9,	last_name...10,	birthdate...11,	ds_dt...12,	diff_date_1, date...13,	diff_date_2, outcome.y,	outcome_date.y)
 
 data_double <- data_double %>% rename_with(~ labels[.])
 openxlsx::write.xlsx(data_double, 'data.xlsx')
